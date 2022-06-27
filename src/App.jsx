8f5a1edb8569a24bb3544eb24076d991e99f3985
sameer mehra <sameermehra7042@gmail.com>
2022-06-27 15:08:40 +0530

    1st changes to tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const app = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Congratulations Sameer</p>
+    </>
+  );
+};
+
+export default app;
